55e373d2094c4968f5865a68a066ef2f37192b0c
Panditax0902 <juanchos2905@gmail.com>
2022-07-18 21:01:40 -0500

    actividad_1_js_juan_valencia

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let name = "Juan",
   yearOfBirth = 2002,
   ciryOfBirth = "Itagüí";
 
+//Este console.log me muestra por consola un mensaje
+
 console.log(
   `Hola, mi nombre es ${name} ${lastName}, vivo en ${ciryOfBirth} y tengo ${
     2022 - yearOfBirth
