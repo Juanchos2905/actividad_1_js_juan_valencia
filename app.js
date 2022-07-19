86f038cbe47b919e29c7203e7b92d7051c5be304
Panditax0902 <juanchos2905@gmail.com>
2022-07-18 20:59:27 -0500

    actividad_1_js_juan_valencia

---
+++ app.js
@@ -0,0 +1,11 @@
+/*Inicializando variables.*/
+let name = "Juan",
+  lastName = "Valencia",
+  yearOfBirth = 2002,
+  ciryOfBirth = "Itagüí";
+
+console.log(
+  `Hola, mi nombre es ${name} ${lastName}, vivo en ${ciryOfBirth} y tengo ${
+    2022 - yearOfBirth
+  } años`
+);
